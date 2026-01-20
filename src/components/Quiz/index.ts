8b4b7a404c771c42421010e7cceea1a95a85c004
Ramtin Javanmardi <ramme95@gmail.com>
2026-01-20 11:02:18 +0100

    fix(quiz): add missing exports for Phase 3 components
    Export ShortAnswerQuestion, FillInBlankQuestion, and
    SelfAssessmentFeedback from the Quiz barrel file.

--- src/components/Quiz/index.ts
+++ src/components/Quiz/index.ts
@@ -1,5 +1,8 @@
 export { QuizModal } from './QuizModal';
 export { MCQQuestion } from './MCQQuestion';
+export { ShortAnswerQuestion } from './ShortAnswerQuestion';
+export { FillInBlankQuestion } from './FillInBlankQuestion';
 export { QuizFeedback } from './QuizFeedback';
+export { SelfAssessmentFeedback } from './SelfAssessmentFeedback';
 export { GeneratingOverlay } from './GeneratingOverlay';
 export { MilestonePrompt } from './MilestonePrompt';
